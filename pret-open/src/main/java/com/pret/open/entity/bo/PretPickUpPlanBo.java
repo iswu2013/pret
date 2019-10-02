641b373ce3a747eec9801a28e817218c9bbed484
wujingsong <1037216275@qq.com>
2019-10-02 22:06:53 +0800

    first commit

---
+++ pret-open/src/main/java/com/pret/open/entity/bo/PretPickUpPlanBo.java
@@ -0,0 +1,11 @@
+package com.pret.open.entity.bo;
+
+/**
+ * @author wujingsong
+ * @title: PretPickUpPlanBo
+ * @projectName pret
+ * @description: TODO
+ * @date 2019/10/28:15 上午
+ */
+public class PretPickUpPlanBo {
+}
---
+++ pret-open/src/main/java/com/pret/open/schedule/ScheduleService.java
@@ -0,0 +1,11 @@
+package com.pret.open.schedule;
+
+/**
+ * @author wujingsong
+ * @title: ScheduleService
+ * @projectName pret
+ * @description: TODO
+ * @date 2019/10/18:59 下午
+ */
+public class ScheduleService {
+}
