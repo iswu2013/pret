a514fc2f53a7fd92e6731afe0c20c88212dbe576
wujingsong <1037216275@qq.com>
2019-11-02 10:56:59 +0800

    commit!

--- pret-open/src/main/java/com/pret/open/entity/vo/PretTransPlanVo.java
+++ pret-open/src/main/java/com/pret/open/entity/vo/PretTransPlanVo.java
@@ -1,20 +1,23 @@
 package com.pret.open.entity.vo;
 
 import com.pret.api.vo.PageFormVo;
+
 import java.io.Serializable;
-/** 
+
+/**
  * <p>Description: [pretmodel]</p>
  * Created on 2019年09月15日
- * @author  <a href="mailto: 1037216275@qq.com">wujinsong</a>
- * @version 1.0 
+ *
+ * @author <a href="mailto: 1037216275@qq.com">wujinsong</a>
+ * @version 1.0
  * Copyright (c) 2019年 极客城堡
  */
-public class PretTransPlanVo extends PageFormVo implements Serializable{
+public class PretTransPlanVo extends PageFormVo implements Serializable {
     private String eq$venderId;
     private long bw$createTimeLong;
     private long createTimeLongEnd;
     private String eq$customerId;
-    private int eq$status;
+    private int eq$status = -1;
     private String eq$pickupFactoryCd;
 
     public String getEq$venderId() {
